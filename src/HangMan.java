91b38425cc14b0230b8c5c40e4d93883c0df6fdd
BeepingSeath <Beeping.Seath.DKS@gmail.com>
2022-02-01 15:02:12 +0100

    Made the word a function and tried to add guessing function but it did not function.

--- src/HangMan.java
+++ src/HangMan.java
@@ -3,5 +3,6 @@ import javax.swing.*;
 public class HangMan {
     public static void main(String[] args) {
     Methods.Startup();
+    Methods.Guessing();
     }
 }
--- src/Methods.java
+++ src/Methods.java
@@ -1,13 +1,31 @@
 import javax.swing.*;
 
 public class Methods {
-    public static void Startup() {
+    private static String Word() {
         String word = JOptionPane.showInputDialog("What real word will they guess?");
+        return word;
+    }
+
+    public static void Startup() {
+        String word = Methods.Word();
         System.out.println("The word is:" + word.length() + " characters long");
         String hword = "";
         for(int i=0; i < word.length(); i++) {
             hword += '_';
         }
         System.out.println(hword);
+    }
+    public static void Guessing() {
+        String word = Methods.Word();
+        String Guess = JOptionPane.showInputDialog("Guess a letter!");
+        for (int i = 0; i < word.length(); i++) {
+            char GuessW = word.charAt(i);
+            char GuessG = Guess.charAt(i);
+            if (word.charAt(i)== GuessG) {
+                GuessW = GuessG;
+            }
+            System.out.println(word);
+        }
+
     }
 }
